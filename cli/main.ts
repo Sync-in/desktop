a2c29489b95fbfded676b6093fe82da4e802ca93
johaven <johan@sync-in.com>
2026-07-31 23:29:24 +0200

    chore(cli): remove deprecated warning suppression

--- cli/main.ts
+++ cli/main.ts
@@ -5,18 +5,6 @@ import { hideBin } from 'yargs/helpers'
 import { getLogger } from '../core/components/handlers/loggers'
 import { getCMDS } from './cmds'
 
-// Catch deprecated warnings
-const originalEmitWarning = process.emitWarning.bind(process)
-process.emitWarning = ((...args: string[]) => {
-  if (args.indexOf('DEP0169') > -1) {
-    // todo: remove this when fixed
-    // https://github.com/axios/axios/issues/7228
-    // DeprecationWarning: url.parse() used in proxy resolution → Node DEP0169
-    return
-  }
-  return originalEmitWarning(...args)
-}) as any
-
 const logger = getLogger('CommandLine')
 
 const yargs = _yargs(hideBin(process.argv))
